2c9369cad7cbdd688014a834cefb2af15b6a439b
zigzag <zigzag.chen@gmail.com>
2009-12-14 17:45:42 +0800

    Initial commit to ready_for_i18n.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'ready_for_i18n'
+
+class Test::Unit::TestCase
+end
